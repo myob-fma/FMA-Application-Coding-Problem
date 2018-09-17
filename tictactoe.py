8d6c38d366cd07179e681302bdf4a51969d029fb
Ida <ides542@aucklanduni.ac.nz>
2018-09-17 14:30:15 +1200

    added the function to check for a draw and simplified player's move X or O

--- tictactoe.py
+++ tictactoe.py
@@ -9,6 +9,7 @@ class TicTacToe:
     def __init__(self):
         self.board = [['.'  for x in range(3)] for y in range(3)]
         self.player = 1;
+        self.player_move = 'X';
         self.x = 0;
         self.y = 0;
         # TODO: move this to be a list of different error messages defined up top
@@ -31,18 +32,18 @@ class TicTacToe:
 
     # Gets the value the player input
     def get_player_input(self):
-        player_input = input("Player " + str(self.player) + " enter a coord x,y to place your X or enter 'q' to give up: ");
+        player_input = input("Player " + str(self.player) + " enter a coord x,y to place your " + self.player_move + " or enter 'q' to give up: ");
         while player_input != 'q': # Check that the player doesn't want to quit
 
             # Checks that the player entered a valid value
             if (self.check_validity_of_input(player_input)):
-                # Set the X and Y variables, but minus one to make them array index values
 
                 if self.board[self.x][self.y] == '.': # Checks that no one has already placed there
-                    self.add_value_to_position(self.x,self.y);
+                    self.board[self.x][self.y] = self.player_move;
+                    self.increment_player(self)
                     if not self.check_if_they_won():
                         self.print_current_board();
-                    else if self.last_move();
+                    elif self.last_move():
                         print("It's a draw!");
                         exit();
                     else:
@@ -62,6 +63,7 @@ class TicTacToe:
     def check_validity_of_input(self, input_value):
         if len(input_value) == 3:
             if (input_value[1] == ',' and str(input_value[0]).isdigit() and str(input_value[2]).isdigit() ):
+                # Set the X and Y variables by minusing one
                 x = int(input_value[0]) - 1;
                 y = int(input_value[2]) - 1;
                 if (x <= 2 and x >= 0 and y <= 2 and y >= 0):
@@ -71,14 +73,13 @@ class TicTacToe:
         return False;
 
     # Adds the player's X or O (depending on which player) to the board
-    def add_value_to_position(self, x, y):
-        players_board_type = 'X'; # Set default as X for player one
+    def increment_player(self):
         if self.player == 2:
-            players_board_type = 'O';
+            self.player_move = 'O';
             self.player = 1; # Decrement player from 2 to 1
         else:
+            self.player_move = 'X';
             self.player = 2; # Increment player from 1 to 2
-        self.board[x][y] = players_board_type;
 
     #TODO CHECK IF THEY WON
     def check_if_they_won(self):
@@ -105,7 +106,11 @@ class TicTacToe:
 
     #TODO: Check if it's a draw
     def last_move(self):
-        return False;
+        for y in self.board:
+            for x in y:
+                if self.board[x][y] == ".":
+                    return False;
+        return True;
 
 # Functions called to begin the game
 ttt = TicTacToe();
